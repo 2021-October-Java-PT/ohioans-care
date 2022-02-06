cb64116b7d296c359c87d51888e793843e4ab298
JPWilson2021 <wilsonjp805@gmail.com>
2022-02-06 14:57:17 -0500

    food component

---
+++ client-side/js/Components/food.js
@@ -0,0 +1,5 @@
+export default function Food(food) {
+    return `
+      <h1>Dev Test</h1>
+      `;
+  }
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,8 @@
+import Food from "./components/food";
+
+builPage();
 console.log('Client Side is wired up!');
+
+function buildPage() {
+    renderFood();
+}     
