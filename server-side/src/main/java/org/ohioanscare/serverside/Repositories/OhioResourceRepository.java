6f69355d42c828393c1df9fa4bbe2aa7e94581b6
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-08 13:28:50 -0500

    Wired up backend to show resources on Food page; Not fully complete as it's presently going to show ALL resources, including non-food related ones

--- server-side/src/main/java/org/ohioanscare/serverside/Repositories/OhioResourceRepository.java
+++ server-side/src/main/java/org/ohioanscare/serverside/Repositories/OhioResourceRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OhioResourceRepository extends CrudRepository <OhioResource, Long> {
-    OhioResource findOhioResourceById(Long id);
+    OhioResource findOhioResourceByEntityName(String entityName);
 }
