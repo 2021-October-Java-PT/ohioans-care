d3c840a320e902899f1a2ca437db36746c442d9c
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-16 16:08:28 -0500

    WIP: working Search function and region/service

--- client-side/js/functions/Search.js
+++ client-side/js/functions/Search.js
@@ -1,4 +1,4 @@
-import Resources from "../Components/Resources.js";
+import FilteredResource from "../Components/FilteredResource.js";
 import apiHelpers from "../api-helpers/apiHelpers.js";
 
 export default function Search() {
@@ -8,8 +8,51 @@ export default function Search() {
 
     searchSubmitBtn.addEventListener("click", () => {
         const searchString = searchBar.value;
-        apiHelpers.getRequest(`http://localhost:8080/resources/zip-codes/${searchString}`, (resource) => {
-            app.innerHTML = Resources(resource);
+        apiHelpers.getRequest(`http://localhost:8080/resources/zip-codes/${searchString}`, (resources) => {
+            app.innerHTML = Resources(resources);
         });
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = document.querySelector("#app");
+//     const searchBar = document.getElementById("search-bar");
+//     const searchSubmitBtn = document.getElementById("search-submit-btn");
+
+//     app.addEventListener('click', () => {
+//         apiHelpers.getRequest('http://localhost:8080/resources', (resources) => {
+//             // console.log('Resources: ', resources);
+
+//             searchBar.addEventListener('keyup', (e) => {
+//                 const searchString = e.target.value.toLowerCase();
+//                 console.log('Search String: ', searchString);
+
+//                 const filteredResources = resources.filter(resource => resource.address( (address) => address.city.includes(searchString)));
+//                 console.log('Filtered Resources: ', filteredResources);
+
+//                 searchSubmitBtn.addEventListener("click", () => {
+//                     const searchString = searchBar.value;
+//                     // apiHelpers.getRequest(`http://localhost:8080/resources/`, (resource) => {
+//                     app.innerHTML = FilteredResource(filteredResources);
+//                     // });
+//                 });
+//             });
+//         });
+//     });
+// }
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,4 +1,5 @@
 import About from "./components/About.js";
+import FilteredResource from "./Components/FilteredResource.js";
 import Home from "./components/Home.js";
 import Resources from "./Components/Resources.js";
 import Search from "./functions/Search.js"
@@ -14,7 +15,7 @@ function buildPage() {
 
     navAbout();
     navServices();
-    
+
 
     renderNorthwest();
     renderNortheast();
@@ -47,10 +48,9 @@ function navHome() {
 
         Search();
         navAbout();
-        
+
     });
 }
-
 
 function navServices() {
     const aboutElem = document.querySelector(".nav-services");
@@ -69,8 +69,11 @@ function navAbout() {
 function renderNorthwest() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("region-nw")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/regions/northeast", (resource) => {
-                app.innerHTML = Resources(resource);
+            console.log("BUTTON FIRING")
+            apiHelpers.getRequest("http://localhost:8080/resources/regions/northwest", (resources) => {
+                app.innerHTML = Resources(resources);
+                const region = "northwest";
+                filterByRegionAndService(region);
             });
         }
     });
@@ -79,8 +82,10 @@ function renderNorthwest() {
 function renderNortheast() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("region-ne")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/regions/northeast", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/regions/northeast", (resources) => {
+                app.innerHTML = Resources(resources);
+                const region = "northeast";
+                filterByRegionAndService(region);
             });
         }
     });
@@ -89,8 +94,10 @@ function renderNortheast() {
 function renderCentral() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("region-c")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/regions/central", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/regions/central", (resources) => {
+                app.innerHTML = Resources(resources);
+                const region = "central";
+                filterByRegionAndService(region);
             });
         }
     });
@@ -99,8 +106,10 @@ function renderCentral() {
 function renderSouthwest() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("region-sw")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/regions/southwest", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/regions/southwest", (resources) => {
+                app.innerHTML = Resources(resources);
+                const region = "southwest";
+                filterByRegionAndService(region);
             });
         }
     });
@@ -109,18 +118,22 @@ function renderSouthwest() {
 function renderSoutheast() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("region-se")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/regions/southeast", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/regions/southeast", (resources) => {
+                app.innerHTML = Resources(resources);
+                const region = "southeast";
+                filterByRegionAndService(region);
             });
         }
     });
 }
 
+
 function renderFood() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("food-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/food", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/food", (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByService();
             });
         }
     });
@@ -129,8 +142,9 @@ function renderFood() {
 function renderHousing() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("housing-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/housing", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/housing", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -139,8 +153,9 @@ function renderHousing() {
 function renderGoods() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("goods-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/goods", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/goods", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -149,8 +164,9 @@ function renderGoods() {
 function renderTransit() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("transit-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/transit", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/transit", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -159,8 +175,9 @@ function renderTransit() {
 function renderHealth() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("health-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/health", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/health", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -169,8 +186,9 @@ function renderHealth() {
 function renderMoney() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("money-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/money", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/money", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -179,8 +197,9 @@ function renderMoney() {
 function renderCare() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("care-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/care", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/care", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -189,8 +208,9 @@ function renderCare() {
 function renderEducation() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("education-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/education", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/education", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -199,8 +219,9 @@ function renderEducation() {
 function renderWork() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("work-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/work", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/work", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
@@ -209,9 +230,156 @@ function renderWork() {
 function renderLegal() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("legal-resources")) {
-            apiHelpers.getRequest("http://localhost:8080/resources/services/legal", (resource) => {
-                app.innerHTML = Resources(resource);
+            apiHelpers.getRequest("http://localhost:8080/resources/services/legal", (resources) => {
+                app.innerHTML = Resources(resources);
+                filterByService();
             });
         }
     });
+}
+
+function filterByRegionAndService(region) {
+    filterFoodByRegion(region);
+    filterRegionByGoods(region);
+    filterRegionByHousing(region);
+    filterRegionByTransit(region);
+    filterRegionByCare(region);
+    filterRegionByMoney(region);
+    filterRegionByHealth(region);
+    filterRegionByEducation(region);
+    filterRegionByLegal(region);
+    filterRegionByWork(region);
+}
+
+function filterFoodByRegion(region) {
+    const foodBtn = document.querySelector("input[name=filter-food-resources]");
+    foodBtn.addEventListener("change", (event) => {
+        
+        if (event.target.classList.contains("filter-food-resources")) {
+            console.log("BUTTON FIRING");
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/food`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByGoods(region) {
+    const foodBtn = document.querySelector("input[name=filter-goods-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-goods-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/goods`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByHousing(region) {
+    const foodBtn = document.querySelector("input[name=filter-housing-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-housing-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/housing`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByTransit(region) {
+    const foodBtn = document.querySelector("input[name=filter-transit-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-transit-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/transit`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByCare(region) {
+    const foodBtn = document.querySelector("input[name=filter-care-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-care-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/care`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByMoney(region) {
+    const foodBtn = document.querySelector("input[name=filter-money-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-money-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/money`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByHealth(region) {
+    const foodBtn = document.querySelector("input[name=filter-health-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-health-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/health`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByEducation(region) {
+    const foodBtn = document.querySelector("input[name=filter-education-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+
+        if (event.target.classList.contains("filter-education-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/education`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+function filterRegionByLegal(region) {
+    const foodBtn = document.querySelector("input[name=filter-legal-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-legal-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/legal`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
+}
+
+
+function filterRegionByWork(region) {
+    const foodBtn = document.querySelector("input[name=filter-work-resources]");
+    foodBtn.addEventListener("change", (event) => {
+
+        if (event.target.classList.contains("filter-work-resources")) {
+            apiHelpers.getRequest(`http://localhost:8080/resources/regions/${region}/services/work`, (resources) => {
+                app.innerHTML = FilteredResource(resources);
+                filterByRegionAndService(region);
+            });
+        }
+    })
 }
