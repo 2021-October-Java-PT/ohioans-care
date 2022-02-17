11fa961f0ff2d2286deb653a94f38e0843f8ddc6
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-16 22:26:23 -0500

    Search Bar working, now able to search by County, City, or Zip Code; Checkboxes semi work -- you can click on a region and then click on a service checkbox from the filter list (such as food) to see only food resources in the region, however the checkbox does not stay ticked, and you can only filter by one service type at a time

--- client-side/js/functions/Search.js
+++ client-side/js/functions/Search.js
@@ -8,51 +8,10 @@ export default function Search() {
 
     searchSubmitBtn.addEventListener("click", () => {
         const searchString = searchBar.value;
-        apiHelpers.getRequest(`http://localhost:8080/resources/zip-codes/${searchString}`, (resources) => {
-            app.innerHTML = Resources(resources);
+        console.log(searchString);
+        apiHelpers.getRequest(`http://localhost:8080/resources/resource-by-location/${searchString}`, (resources) => {
+            console.log(resources)
+            app.innerHTML = FilteredResource(resources);
         });
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const app = document.querySelector("#app");
-//     const searchBar = document.getElementById("search-bar");
-//     const searchSubmitBtn = document.getElementById("search-submit-btn");
-
-//     app.addEventListener('click', () => {
-//         apiHelpers.getRequest('http://localhost:8080/resources', (resources) => {
-//             // console.log('Resources: ', resources);
-
-//             searchBar.addEventListener('keyup', (e) => {
-//                 const searchString = e.target.value.toLowerCase();
-//                 console.log('Search String: ', searchString);
-
-//                 const filteredResources = resources.filter(resource => resource.address( (address) => address.city.includes(searchString)));
-//                 console.log('Filtered Resources: ', filteredResources);
-
-//                 searchSubmitBtn.addEventListener("click", () => {
-//                     const searchString = searchBar.value;
-//                     // apiHelpers.getRequest(`http://localhost:8080/resources/`, (resource) => {
-//                     app.innerHTML = FilteredResource(filteredResources);
-//                     // });
-//                 });
-//             });
-//         });
-//     });
-// }
