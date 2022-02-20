84beb13719b0b80b0e30384dbb67e34ae06f1b84
twillis90 <t.willis9290@gmail.com>
2022-02-20 16:29:28 -0500

    more updates to user profiles

---
+++ client-side/js/Components/Profile.js
@@ -0,0 +1,12 @@
+export default function UserProfile(userProfile) {
+    console.log(userProfile);
+    // return `
+    // <div>
+    // <ul>
+    // <li>${}</li>
+    // <li></li>
+    // <li></li>
+    // </ul>
+    // </div>
+    // `
+}
