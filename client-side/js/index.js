f422124bc0924454e577335f577ec4921bdcf06d
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-20 10:39:34 -0500

    Fixed SearchBar to work with Filters

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,12 +1,9 @@
-import About from "./Components/About.js";
-import FiltersApp from "./Components/FiltersApp.js";
-import Home from "./Components/Home.js";
-import OhioResources from "./Components/OhioResources.js"
-import Resources from "./Components/Resources.js";
-import Search from "./functions/Search.js"
-import Services from "./Components/Services.js";
+import About from "./components/About.js";
+import FiltersApp from "./components/FiltersApp.js";
+import Home from "./components/Home.js";
+import OhioResources from "./components/OhioResources.js"
+import Services from "./components/Services.js";
 import apiHelpers from "./api-helpers/apiHelpers.js";
-;
 
 const app = document.querySelector("#app");
 
@@ -68,6 +65,49 @@ function navAbout() {
     });
 }
 
+function Search() {
+    const app = document.querySelector("#app");
+    const searchBar = document.getElementById("search-bar");
+    const searchSubmitBtn = document.getElementById("search-submit-btn");
+
+    searchSubmitBtn.addEventListener("click", () => {
+        const searchString = searchBar.value;
+        console.log(searchString);
+        apiHelpers.getRequest(`http://localhost:8080/resources/resource-by-location/${searchString}`, (resources) => {
+            console.log(resources)
+            app.innerHTML = FiltersApp();
+            const resourcesContainer = document.getElementById("resource-container");
+            resourcesContainer.innerHTML = OhioResources(resources);
+            filterResourcesByService(resources, resourcesContainer);
+        });
+    });
+
+    window.addEventListener("keydown", function (event) {
+        if (event.defaultPrevented) {
+            return;
+        }
+
+        switch (event.key) {
+            case "Enter":
+                const searchString = searchBar.value;
+                console.log(searchString);
+                apiHelpers.getRequest(`http://localhost:8080/resources/resource-by-location/${searchString}`, (resources) => {
+                    console.log(resources)
+                    app.innerHTML = FiltersApp();
+                    const resourcesContainer = document.getElementById("resource-container");
+                    resourcesContainer.innerHTML = OhioResources(resources);
+                    filterResourcesByService(resources, resourcesContainer);
+                });
+                break;
+
+            default:
+                return;
+        }
+
+        event.preventDefault();
+    }, true);
+}
+
 function renderNorthwest() {
     app.addEventListener("click", (event) => {
         if (event.target.classList.contains("region-nw")) {
@@ -76,7 +116,8 @@ function renderNorthwest() {
                 const resourcesContainer = document.getElementById("resource-container");
                 resourcesContainer.innerHTML = OhioResources(resources);
                 const location = "northwest";
-                filterByLocationAndService(location);
+                // filterByLocationAndService(location);
+                filterResourcesByService(resources, resourcesContainer);
             });
         }
     });
@@ -90,7 +131,8 @@ function renderNortheast() {
                 const resourcesContainer = document.getElementById("resource-container");
                 resourcesContainer.innerHTML = OhioResources(resources);
                 const location = "northeast";
-                filterByLocationAndService(location);
+                // filterByLocationAndService(location);
+                filterResourcesByService(resources, resourcesContainer);
             });
         }
     });
@@ -104,7 +146,8 @@ function renderCentral() {
                 const resourcesContainer = document.getElementById("resource-container");
                 resourcesContainer.innerHTML = OhioResources(resources);
                 const location = "central";
-                filterByLocationAndService(location);
+                // filterByLocationAndService(location);
+                filterResourcesByService(resources, resourcesContainer);
             });
         }
     });
@@ -118,7 +161,8 @@ function renderSouthwest() {
                 const resourcesContainer = document.getElementById("resource-container");
                 resourcesContainer.innerHTML = OhioResources(resources);
                 const location = "southwest";
-                filterByLocationAndService(location);
+                // filterByLocationAndService(location);
+                filterResourcesByService(resources, resourcesContainer);
             });
         }
     });
@@ -139,102 +183,6 @@ function renderSoutheast() {
     });
 }
 
-function filterResourcesByService(resources, resourcesContainer) {
-    let filteredResources = []
-    const filterBtn = document.querySelector("#filter-btn");
-    const careCheckBox = document.querySelector("#filter-care-resources");
-    const educationCheckBox = document.querySelector("#filter-education-resources");
-    const foodCheckBox = document.querySelector("#filter-food-resources");
-    const goodsCheckBox = document.querySelector("#filter-goods-resources");
-    const healthCheckBox = document.querySelector("#filter-health-resources");
-    const housingCheckBox = document.querySelector("#filter-housing-resources");
-    const legalCheckBox = document.querySelector("#filter-legal-resources");
-    const moneyCheckBox = document.querySelector("#filter-money-resources");
-    const transitCheckBox = document.querySelector("#filter-transit-resources");
-    const workCheckBox = document.querySelector("#filter-work-resources");
-    let care = "";
-    let food = "";
-    let education = "";
-    let goods = "";
-    let health = "";
-    let housing = "";
-    let legal = "";
-    let money = "";
-    let transit = "";
-    let work = "";
-
-    filterBtn.addEventListener("click", (event) => {
-        if (careCheckBox.checked) {
-            care = "care";
-        } else {
-            care = "";
-        }
-        if (educationCheckBox.checked) {
-            education = "education";
-        } else {
-            education = "";
-        }
-        if (foodCheckBox.checked) {
-            food = "food";
-        } else {
-            food = "";
-        }
-        if (goodsCheckBox.checked) {
-            goods = "goods";
-        } else {
-            goods = "";
-        }
-        if (healthCheckBox.checked) {
-            health = "health";
-        } else {
-            health = "";
-        }
-        if (housingCheckBox.checked) {
-            housing = "housing";
-        } else {
-            housing = "";
-        }
-        if (legalCheckBox.checked) {
-            legal = "legal";
-        } else {
-            legal = "";
-        }
-        if (moneyCheckBox.checked) {
-            money = "money";
-        } else {
-            money = "";
-        }
-        if (transitCheckBox.checked) {
-            transit = "transit";
-        } else {
-            transit = "";
-        }
-        if (workCheckBox.checked) {
-            work = "work";
-        } else {
-            work = "";
-        }
-        resources.map(resource => {
-            resource.services.map(service => {
-                if (service.service.toLocaleLowerCase() === care ||
-                    service.service.toLocaleLowerCase() === education ||
-                    service.service.toLocaleLowerCase() === food ||
-                    service.service.toLocaleLowerCase() === goods ||
-                    service.service.toLocaleLowerCase() === health ||
-                    service.service.toLocaleLowerCase() === housing ||
-                    service.service.toLocaleLowerCase() === legal ||
-                    service.service.toLocaleLowerCase() === money ||
-                    service.service.toLocaleLowerCase() === transit ||
-                    service.service.toLocaleLowerCase() === work) {
-                    filteredResources.push(resource);
-                }
-            })
-        })
-        console.log(filteredResources);
-        resourcesContainer.innerHTML = OhioResources(filteredResources.length ? filteredResources : resources);
-        filteredResources = [];
-    })
-}
 
 function renderFood() {
     app.addEventListener("click", (event) => {
@@ -365,6 +313,103 @@ function renderLegal() {
             });
         }
     });
+}
+
+function filterResourcesByService(resources, resourcesContainer) {
+    let filteredResources = []
+    const filterBtn = document.querySelector("#filter-btn");
+    const careCheckBox = document.querySelector("#filter-care-resources");
+    const educationCheckBox = document.querySelector("#filter-education-resources");
+    const foodCheckBox = document.querySelector("#filter-food-resources");
+    const goodsCheckBox = document.querySelector("#filter-goods-resources");
+    const healthCheckBox = document.querySelector("#filter-health-resources");
+    const housingCheckBox = document.querySelector("#filter-housing-resources");
+    const legalCheckBox = document.querySelector("#filter-legal-resources");
+    const moneyCheckBox = document.querySelector("#filter-money-resources");
+    const transitCheckBox = document.querySelector("#filter-transit-resources");
+    const workCheckBox = document.querySelector("#filter-work-resources");
+    let care = "";
+    let food = "";
+    let education = "";
+    let goods = "";
+    let health = "";
+    let housing = "";
+    let legal = "";
+    let money = "";
+    let transit = "";
+    let work = "";
+
+    filterBtn.addEventListener("click", (event) => {
+        if (careCheckBox.checked) {
+            care = "care";
+        } else {
+            care = "";
+        }
+        if (educationCheckBox.checked) {
+            education = "education";
+        } else {
+            education = "";
+        }
+        if (foodCheckBox.checked) {
+            food = "food";
+        } else {
+            food = "";
+        }
+        if (goodsCheckBox.checked) {
+            goods = "goods";
+        } else {
+            goods = "";
+        }
+        if (healthCheckBox.checked) {
+            health = "health";
+        } else {
+            health = "";
+        }
+        if (housingCheckBox.checked) {
+            housing = "housing";
+        } else {
+            housing = "";
+        }
+        if (legalCheckBox.checked) {
+            legal = "legal";
+        } else {
+            legal = "";
+        }
+        if (moneyCheckBox.checked) {
+            money = "money";
+        } else {
+            money = "";
+        }
+        if (transitCheckBox.checked) {
+            transit = "transit";
+        } else {
+            transit = "";
+        }
+        if (workCheckBox.checked) {
+            work = "work";
+        } else {
+            work = "";
+        }
+        resources.map(resource => {
+            resource.services.map(service => {
+                if (service.service.toLocaleLowerCase() === care ||
+                    service.service.toLocaleLowerCase() === education ||
+                    service.service.toLocaleLowerCase() === food ||
+                    service.service.toLocaleLowerCase() === goods ||
+                    service.service.toLocaleLowerCase() === health ||
+                    service.service.toLocaleLowerCase() === housing ||
+                    service.service.toLocaleLowerCase() === legal ||
+                    service.service.toLocaleLowerCase() === money ||
+                    service.service.toLocaleLowerCase() === transit ||
+                    service.service.toLocaleLowerCase() === work) {
+                    filteredResources.push(resource);
+                }
+            })
+        })
+        console.log(filteredResources);
+        resourcesContainer.innerHTML = OhioResources(filteredResources.length ? filteredResources : resources);
+        filteredResources = [];
+    })
 }
 
 // function filterByLocationAndService(location) {
