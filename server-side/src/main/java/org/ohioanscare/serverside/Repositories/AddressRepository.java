c29a63bc7886e9d03dcc1bfc62cc8893a0cd9eb7
George Avent <gaventjr@gmail.com>
2022-02-27 12:46:37 -0500

    FinalController

--- server-side/src/main/java/org/ohioanscare/serverside/Repositories/AddressRepository.java
+++ server-side/src/main/java/org/ohioanscare/serverside/Repositories/AddressRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
     Address findByCityContains(String city);
+    Address findByCounty_CountyContainsIgnoreCase(String county);
 }
---
+++ server-side/src/main/java/org/ohioanscare/serverside/rest/Controllers/AddressRestController.java
@@ -0,0 +1,30 @@
+package org.ohioanscare.serverside.rest.Controllers;
+
+import org.ohioanscare.serverside.Models.Address;
+import org.ohioanscare.serverside.Repositories.AddressRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+
+@RestController
+@CrossOrigin
+public class AddressRestController {
+
+    @Resource
+    private AddressRepository addressRepo;
+
+    @GetMapping("/addresses")
+    public Collection<Address> getAllAddresses() {
+        return (Collection<Address>) addressRepo.findAll();
+    }
+
+    @GetMapping("/addresses/{countyName}")
+    public Address getAddressByCounty(@PathVariable String countyName) {
+        return addressRepo.findByCounty_CountyContainsIgnoreCase(countyName);
+    }
+
+}
