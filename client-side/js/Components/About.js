f63bb77947e3d7486004f6b989a3f015e18fa0b6
bradymgasser <bradymgasser@gmail.com>
2022-03-03 17:36:11 -0500

    updated paragraph

--- client-side/js/Components/About.js
+++ client-side/js/Components/About.js
@@ -14,13 +14,13 @@ export default function About() {
             <h4>OUR STORY</h4>
             <div class="serv-cont">
             <p class="serv-cont-par">
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
-            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
-            nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
-            vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            Ohioans Care is a central, integrated space where every Ohioan can find and offer support for all aspects of life. 
+            Too often, our friends and neighbors struggle through no fault of their own. Navigating the information landscape should be one burden they never have to face.
+            The concept was born from the mind of our very own Jo, who as a social service worker, saw too many of her clients miss out on available help due to the disorganization of resources across the state.
+            Here, users can search across a variety of services in their zip code and suggest resources that are tucked away or easily overlooked in their area.
+            Additionally, users can access the community page to participate in discussions with their neighbors; whether it's to introduce yourself after moving or to coordinate snow removal efforts on your block.
+            On Ohioans Care, your heart is always in the right place.
+
             </p>
         </div>
     </div>
